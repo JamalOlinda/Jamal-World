ad33d59f7b3755ad4eea7f934d4b0478d192ef85
João Victor Pacheco <162443566+JamalOlinda@users.noreply.github.com>
2024-06-19 09:46:13 -0300

    Add files via upload

---
+++ projeto javascript/js/index.js
@@ -0,0 +1,11 @@
+let topMod = 0;
+let leftMod = 0;
+
+$("body").on("mousemove", webHandler);
+function webHandler(event){
+    let button = $('#no') .position();
+    let buttonCenter = {
+        x: button.left + 50,
+        y: button.top + 20
+     }
+}
